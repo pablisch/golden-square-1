798bea75a123bc1f0c76b0a50ad7d3e62e960208
Pablo <pablito@me.com>
2023-03-14 15:14:22 +0000

    Added tests for check_codeword 2 & 3. Added report_length.rb

--- testing-bites/lib/add_five.rb
+++ testing-bites/lib/add_five.rb
@@ -1,3 +1,3 @@
 def add_five(num)
-  return num + 4
+  return num + 5
 end 
--- testing-bites/spec/check_codeword_spec.rb
+++ testing-bites/spec/check_codeword_spec.rb
@@ -2,8 +2,20 @@ require "check_codeword"
 
 RSpec.describe "check_codeword method" do
   context "codeword matches horse" do
-    it "returns greeting when passed horse" do
+    it "returns greeting when passed 'horse'" do
       expect(check_codeword("horse")).to eq "Correct! Come in."
+    end
+  end
+
+  context "codeword first char 'h', last char 'e' and isn't horse" do
+    it "returns 'close' message when passed 'house'" do
+      expect(check_codeword("house")).to eq "Close, but nope."
+    end
+  end
+
+  context "codeword doesn't include first char 'h', last char 'e' or isn't horse" do
+    it "returns 'wrong' message when passed 'fools'" do
+      expect(check_codeword("fools")).to eq "WRONG!"
     end
   end
 end
