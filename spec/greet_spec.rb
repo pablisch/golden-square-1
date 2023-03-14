0d4d90cbcf1f76797748ac7a3ce1d4ad9e4d4993
Pablo <pablito@me.com>
2023-03-14 12:43:54 +0000

    Added another grret test

--- spec/greet_spec.rb
+++ spec/greet_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe "greet method" do
   it "returns 'Hello, Pablo!' when passed 'Pablo'" do
     expect(greet("Pablo")).to eq "Hello, Pablo!"
   end
+
+  it "returns 'Hello, Sabina!' when passed 'Sabina'" do
+    expect(greet("Sabina")).to eq "Hello, Sabina!"
+  end
 end
