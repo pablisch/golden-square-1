c60ac4528d9d87d3de4ba068814e0f2574635c7b
Pablo <pablito@me.com>
2023-03-14 13:31:18 +0000

    codeword two tests completed

--- spec/check_codeword_spec.rb
+++ spec/check_codeword_spec.rb
@@ -2,7 +2,15 @@ require "check_codeword"
 
 RSpec.describe "check_codeword method" do
   context "Codeword is correct" do
-    it ""
+    it "returns greeting when codeword is 'horse" do
+      expect(check_codeword("horse")).to eq "Correct! Come in."
+    end
+  end
+
+  context "Codeword starts with 'h', ends with'e' but is not 'horse" do
+    it "returns 'close' message when passed 'house'" do
+      expect(check_codeword("house")).to eq "Close, but nope."
+    end
   end
 
   
