57b4f6c9a2a5da46781fe6bcaa45632cf6c83e85
Pablo <pablito@me.com>
2023-03-14 22:22:11 +0000

    added first test for string builder

--- testing-bites/spec/string_builder_spec.rb
+++ testing-bites/spec/string_builder_spec.rb
@@ -1,2 +1,9 @@
 require "string_builder"
 
+RSpec.describe StringBuilder do
+  it "returns a string length of 4 when funk is added" do
+    string = StringBuilder.new
+    string.add("funk")
+    expect(string.size).to eq 4
+  end
+end
