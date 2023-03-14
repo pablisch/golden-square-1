f1ff1a3a1cc62c7ab80ac5679914c928dd4b45ff
Pablo <pablito@me.com>
2023-03-14 06:39:26 +0000

    changed syntax for dairy spec to use context..do

--- spec/diary_spec.rb
+++ spec/diary_spec.rb
@@ -2,30 +2,42 @@ require "diary"
 
 describe "diary make_snippet method" do
   describe "test strings containing under six words" do
-    it "make_snippet('Hello World!') returns same" do
-      expect(make_snippet("Hello World!")).to eq "Hello World!"
+    context "given 'Hello World!'" do
+      it "returns 'Hello World!'" do
+        expect(make_snippet("Hello World!")).to eq "Hello World!"
+      end
     end
 
-    it "make_snippet('') returns same" do
-      expect(make_snippet("")).to eq ""
+    context "given an empty string" do
+      it "returns an empty string" do
+        expect(make_snippet("")).to eq ""
+      end
     end
 
-    it "make_snippet('one two three four five') returns same" do
-      expect(make_snippet("one two three four five")).to eq "one two three four five"
+    context "given 'one two three four five'" do
+      it "returns 'one two three four five'" do
+        expect(make_snippet("one two three four five")).to eq "one two three four five"
+      end
     end
   end
 
   describe "test strings containing over five words" do
-    it "make_snippet('Hello World!') returns same" do
-      expect(make_snippet("Hello World! How are you today")).to eq "Hello World! How are you..."
+    context "given 'Hello World! How are you today?'" do
+      it "returns 'Hello World! How are you...'" do
+        expect(make_snippet("Hello World! How are you today?")).to eq "Hello World! How are you..."
+      end
     end
 
-    it "make_snippet('In this project you will build personal diary system.') returns same" do
-      expect(make_snippet("In this project you will build personal diary system.")).to eq "In this project you will..."
+    context "given 'In this project you will build personal diary system.'" do
+      it "returns 'In this project you will...'" do
+        expect(make_snippet("In this project you will build personal diary system.")).to eq "In this project you will..."
+      end
     end
 
-    it "Whole paragraph returns same" do
-      expect(make_snippet("Test-driven development is an approach to building software in the same way that Karate is an approach to combat. It structures everything you do. At first it will feel strange and you will want to resume your unstructured approach. However, it allows you to tackle much greater problems.")).to eq "Test-driven development is an approach..."
+    context "given a whole paragraph" do
+      it "returns the first five words plus '...'" do
+        expect(make_snippet("Test-driven development is an approach to building software in the same way that Karate is an approach to combat. It structures everything you do. At first it will feel strange and you will want to resume your unstructured approach. However, it allows you to tackle much greater problems.")).to eq "Test-driven development is an approach..."
+      end
     end
   end
 end
--- spec/word_counter_spec.rb
+++ spec/word_counter_spec.rb
@@ -1,7 +1,19 @@
 require "word_counter"
 
 describe "count_words method" do
-  it "returns one when one word is given" do
+  it "returns 1 when one word is given" do
     expect(count_words("one")).to eq 1
   end 
+
+  it "returns 5 when five words are given" do
+    expect(count_words("one two three four five")).to eq 5
+  end 
+
+  it "returns 0 when no words are given" do
+    expect(count_words("")).to eq 0
+  end 
+
+  it "returns 2 when 'one    two    ' given" do
+    expect(count_words("one    two    ")).to eq 2
+  end
 end
