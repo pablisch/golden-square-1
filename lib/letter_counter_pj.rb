41d4d4c9a9c577ae245d31b50d7273bec73144ae
Pablo <pablito@me.com>
2023-03-20 12:42:20 +0000

    solved letter counter debug

--- lib/letter_counter_pj.rb
+++ lib/letter_counter_pj.rb
@@ -4,16 +4,17 @@ class LetterCounter
   end
 
   def calculate_most_common()
-    counter = Hash.new(1)
+    counter = Hash.new(0)
     most_common = nil
     most_common_count = 0
+    # binding.irb
     @text.chars.each do |char|
-      binding.irb
+      # binding.irb
       next unless is_letter?(char)
       counter[char] = (counter[char] || 0) + 1
       if counter[char] > most_common_count
         most_common = char
-        most_common_count += counter[char]
+        most_common_count += 1
       end
       # binding.irb
     end
