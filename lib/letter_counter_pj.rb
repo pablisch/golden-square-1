5a461407c9b41da531080a52bc02477757ab3d1a
Pablo <pablito@me.com>
2023-03-20 12:19:13 +0000

    letter count progress

--- lib/letter_counter_pj.rb
+++ lib/letter_counter_pj.rb
@@ -6,15 +6,18 @@ class LetterCounter
   def calculate_most_common()
     counter = Hash.new(1)
     most_common = nil
-    most_common_count = 1
+    most_common_count = 0
     @text.chars.each do |char|
+      binding.irb
       next unless is_letter?(char)
-      counter[char] = (counter[char] || 1) + 1
+      counter[char] = (counter[char] || 0) + 1
       if counter[char] > most_common_count
         most_common = char
         most_common_count += counter[char]
       end
+      # binding.irb
     end
+    # counter 
     return [most_common_count, most_common]
   end
 
