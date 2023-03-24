8f0fa1794dc5a8d345675b6fc2a76470cb8a3971
Pablo <pablito@me.com>
2023-03-24 02:29:38 +0000

    gold-mock-3 challenge added to gold-mock-1 challange
    These two challenges are combined.
    mock3 is an addition to the mock1 task challenge
    files added and completed.

--- gold-mock-1-challenge/spec/tasks_integration_spec.rb
+++ gold-mock-1-challenge/spec/tasks_integration_spec.rb
@@ -1,5 +1,6 @@
 require "task_list"
 require "task"
+require "task_formatter"
 
 RSpec.describe "tasks integration" do
   it "adds tasks to a list" do
@@ -20,5 +21,18 @@ RSpec.describe "tasks integration" do
     task_1.mark_complete
     task_2.mark_complete
     expect(task_list.all_complete?).to eq true
+  end
+
+  it "returns a formatted task string for a not completed task" do
+    task_1 = Task.new("Walk the dog")
+    formatter = TaskFormatter.new(task_1)
+    expect(formatter.format).to eq "- [ ] Walk the dog"
+  end
+
+  it "returns a formatted task string for a completed task" do
+    task_1 = Task.new("Sleep")
+    formatter = TaskFormatter.new(task_1)
+    task_1.mark_complete
+    expect(formatter.format).to eq "- [x] Sleep"
   end
 end
