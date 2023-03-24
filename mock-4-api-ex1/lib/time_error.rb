0fdb9e7244a28e926b8150bcdbf1811efb2ad372
Pablo <pablito@me.com>
2023-03-24 12:35:46 +0000

    completed mock 4 challenge

--- mock-4-api-ex1/lib/time_error.rb
+++ mock-4-api-ex1/lib/time_error.rb
@@ -5,15 +5,24 @@ require 'net/http'
 class TimeError
   # Returns difference in seconds between server time
   # and the time on this computer
+  def initialize(requester, time)
+    @requester = requester
+    @time = time
+  end
+
   def error
-    return get_server_time - Time.now
+    return get_server_time - @time
   end
 
   private
 
   def get_server_time
-    text_response = Net::HTTP.get(URI("https://worldtimeapi.org/api/ip"))
+    text_response = @requester.get(URI("https://worldtimeapi.org/api/ip"))
     json = JSON.parse(text_response)
     return DateTime.parse(json["utc_datetime"]).to_time
   end
 end
+
+# USAGE
+time_error = TimeError.new(Net::HTTP, Time.now)
+p time_error.error
